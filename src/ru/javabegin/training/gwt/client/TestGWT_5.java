395595d3e7585c73913e5c2944429fd82c44e4a2
Roman Brovko <brovko-roman@ya.ru>
2017-09-20 22:03:28 +0300

    lesson 5 home work

--- src/ru/javabegin/training/gwt/client/TestGWT_5.java
+++ src/ru/javabegin/training/gwt/client/TestGWT_5.java
@@ -3,58 +3,49 @@ package ru.javabegin.training.gwt.client;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.KeyPressEvent;
+import com.google.gwt.event.dom.client.KeyPressHandler;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
-public class TestGWT_5 implements EntryPoint {
+public class TestGWT_5 implements EntryPoint, ClickHandler, KeyPressHandler {
 
 	private VerticalPanel mainPanel = new VerticalPanel();
 
 	private Button button1 = new Button("Button 1");
-	private Button button2 = new Button("Button 2");
-	private Button button3 = new Button("Button 3");
+	private TextBox textBox = new TextBox();
+	private Label label = new Label();
 
 	@Override
 	public void onModuleLoad() {
 
-		button1.addClickHandler(new ClickHandler() {
-
-			@Override
-			public void onClick(ClickEvent event) {
-				buttonClick(event.getSource().toString());
-			}
-		});
-
-		button2.addClickHandler(new ClickHandler() {
-
-			@Override
-			public void onClick(ClickEvent event) {
-				buttonClick(event.getSource().toString());
-			}
-		});
-
-		button3.addClickHandler(new ClickHandler() {
-
-			@Override
-			public void onClick(ClickEvent event) {
-				buttonClick(event.getSource().toString());
-			}
-		});
-
+		button1.addClickHandler(this);
 		button1.setStyleName("redButton");
 
+		textBox.addKeyPressHandler(this);
+
+		label.setStyleName("counter");
+
 		mainPanel.add(button1);
-		mainPanel.add(button2);
-		mainPanel.add(button3);
+		mainPanel.add(textBox);
+		mainPanel.add(label);
 
 		RootPanel.get("mainContainer").add(mainPanel);
 
 	}
 
-	private void buttonClick(String source) {
-		Window.alert(source);
+	@Override
+	public void onClick(ClickEvent event) {
+		Window.alert(event.getSource().toString());
+	}
+
+	@Override
+	public void onKeyPress(KeyPressEvent event) {
+		label.setText(String.valueOf(textBox.getText().trim().length()));
 	}
 
 }
